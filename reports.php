cf4503490cd234ee6ba2266dcc7f970a13198d11
KSURESHCHANDRAREDDY <119147501+KSURESHCHANDRAREDDY@users.noreply.github.com>
2025-05-26 21:31:29 +0530

    Update reports.php

--- reports.php
+++ reports.php
@@ -246,7 +246,7 @@
         }
 
         // Database connection
-        $conn = new mysqli('localhost:4306', 'root', '', 'unauthorizedconstructions');
+        $conn = new mysqli('localhost', '', '', '');
 
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
